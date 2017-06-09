cdaab140a38e4147fceef82d17e05dc1760a2ff5
Mounika <mounika.tunga9@gmail.com>
2017-06-08 17:24:21 -0700

    changes made to py file

--- myname.py
+++ myname.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -6,8 +6,8 @@ def index():
 @app.route('/process', methods=['POST'])
 def create_user():
    print "Got Post Info"  
+   print request.form
    name = request.form['name']
-   email = request.form['email']
    # redirects back to the '/' route
    return redirect('/')
 app.run(debug=True) 
